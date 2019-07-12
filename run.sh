3737ab54af014e7600fe75b212dd5491ae02ef07
yt8492 <tmymyut2000@gmail.com>
2019-07-12 18:27:19 +0900

    find bug

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo $DB_URL
 ./gradlew shadowJar
-java -jar /KtorHelloWorld/build/libs/KtorHelloWorld-1.0.6-all.jar
+java -jar /KtorHelloWorld/build/libs/KtorHelloWorld-1.0.6-checkbug-all.jar
